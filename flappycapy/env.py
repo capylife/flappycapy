7d9d329c2943e405b39ef6c7410846f908382353
WardPearce <wardpearce@protonmail.com>
2022-04-15 16:58:53 +1200

    Changed default for CAPY_API_LINK

--- flappycapy/env.py
+++ flappycapy/env.py
@@ -12,7 +12,7 @@ ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
 CHECK_DELAY = int(os.getenv("CHECK_DELAY", 120))
 CAPY_LIFE_LINK = os.getenv("CAPY_LIFE_LINK", "https://capy.life")
-CAPY_API_LINK = os.getenv("CAPY_API_LINK", "http://localhost/api/")
+CAPY_API_LINK = os.getenv("CAPY_API_LINK", "https://capy.life/api/")
 
 MONGO_HOST = os.getenv("MONGO_IP", "localhost")
 MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
